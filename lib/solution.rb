fe877f8536e2dfb257dcf96d7459b27c7a16f516
thesebonesalone <duikhead2@gmail.com>
2020-09-29 09:37:42 -0500

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 def clock_angle(time)
-    # code your solution here
+    time_array = time.split(':')
+    #binding.pry
+    time_array = time_array.map{|value| value.to_i}
+    time_array[0] = time_array[0] * 30 + 0.5 * time_array[1]
+    time_array[1] = time_array[1] * 6
+    time_array = time_array.map do |value|
+        if value == 360
+            value = 0
+        end
+        value
+    end
+
+
+    # puts time_array
+    # result = time_array[0] - time_array[1]
+    # if result < 0
+    #     return 360 + result
+    # else
+    #     return result
+    # end
+
+    time_array[0] >= time_array[1] ? time_array[0] - time_array[1] : 360 - (time_array[1] - time_array[0])
+
 end
